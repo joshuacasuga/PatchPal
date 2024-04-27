93b9c683e1f58e585d072d49d865c56fc6d5538e
Joshua Casuga <jomatthew8@gmail.com>
2024-04-27 15:18:38 -0700

    api calls and pages

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from flask import Flask, request, url_for, session, redirect, render_template
+
+load_dotenv(".env")
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(debug=True)
+
+api = OpenAI(api_key=OPENAI_API_KEY)
+
+def get_response(user_input):
+    response = api.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Act as a nurse who gives suggestions for first aid."},
+            {"role": "user", "content": user_input}
+        ])
+    return response.choices[0].message.content
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="First Aid")
+
+@app.route('/result', methods=['POST'])
+def get_result():
+    query = request.form['search_query']
+    response = get_response(query)
+    #result = response.choices[0].message.content
+    return render_template('result.html', result=response)
+
+#user_input = input()
+#print(get_response(user_input))
